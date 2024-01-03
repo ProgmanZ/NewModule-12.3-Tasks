9a6060a9f69def4b6f4912a632590d2a7cba1abf
ProgmanZ <dumphill@gmail.com>
2024-01-03 23:40:34 +0300

    Task-12.3.2 - Готово

--- Task-12.3.2.py
+++ Task-12.3.2.py
@@ -4,28 +4,53 @@ class Robots:
         self._model = model
         self._action = action
 
-    def _operate(self):
-        return {self._action}
-
-    def __str__(self):
-        return f"Робот: {self._model} Сейчас делает: {self._operate():}."
+    def operate(self):
+        print(f"\nРобот сейчас {self._action}.")
 
 
 class DusterRobots(Robots):
 
-    def __init__(self, model, action, bunker=0):
+    def __init__(self, model, action="пылесосит пол", bunker=0):
         super().__init__(model, action)
-        self.bunker = bunker
+        self._bunker = bunker
 
-    def _operate(self):
-        self.bunker += 10
-        return f"{self.__str__()}\nЗаполненность мешка сейчас:{self.bunker}."
+    def operate(self):
+        super().operate()
+        print(f"Текущая заполняемость мешка = {self._bunker}.")
+
 
 
 class WarRobots(Robots):
 
-    def __init__(self, model, deep, action):
-        super().__init__(model)
-        self.deep = deep
-        self.action = action
+    def __init__(self, model, guns, action="защищает военный объект.", ):
+        super().__init__(model, action)
+        self._guns = guns
+
+    def operate(self):
+        super().operate()
+        print(f"С помощью {self._guns} пушек.")
+
+
+class WarSheeps(WarRobots):
+
+    def __init__(self, model, guns, deep):
+        super().__init__(model, guns)
+        self._deep = deep
+
+    def operate(self):
+        super().operate()
+        print(f"Охрана ведется на глубине {self._deep} метров.")
+
+
+dustR = DusterRobots("Dust")
+warR = WarRobots("WarR",10)
+sheepR = WarSheeps("DeepWarR", 30,4000)
+
+dustR.operate()
+warR.operate()
+sheepR.operate()
+
+
+
+
 
